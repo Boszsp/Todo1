28ee66e986506ca97650ab4bf23b143d36486e3d
Supassssss <112678332+SupaschaiPh@users.noreply.github.com>
2024-03-23 01:54:42 +0700

    feat:todo 1

---
+++ src/lib/custom/TodoCard.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Button from '$lib/components/ui/button/button.svelte';
+	import Card from '$lib/components/ui/card/card.svelte';
+	import Checkbox from '$lib/components/ui/checkbox/checkbox.svelte';
+	import Label from '$lib/components/ui/label/label.svelte';
+    import { toast } from "svelte-sonner";
+
+	export let title = '';
+	export let checked = false;
+    let cid = title+Math.floor(Math.random()*100)
+
+    const copyToCliboard = ()=>{
+        navigator.clipboard.writeText(title).then(
+            ()=>{
+                toast.success("Copy \""+title+"\" to clipboard success")
+
+            }
+        ).catch(
+            ()=>{
+                toast.error("Cannot copy \""+title+"\" to clipboard success")
+
+            }
+        )
+    }
+</script>
+
+<Card>
+	<div class="m-2 mx-4 flex items-center gap-2">
+		<Checkbox bind:checked id={cid}></Checkbox>
+		<Label for={cid} class="text-md w-full">{title}</Label>
+		<Button on:click={copyToCliboard} class="self-end" variant="outline">Copy</Button>
+    </div>
+</Card>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,57 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import * as Card from '$lib/components/ui/card';
+	import CardContent from '$lib/components/ui/card/card-content.svelte';
+	import TodoCard from '$lib/custom/TodoCard.svelte';
+	import { blur } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
+	import { dndzone } from 'svelte-dnd-action';
+	let items = [
+		{ id: 1, name: 'item1' ,checked:true},
+		{ id: 2, name: 'item2' ,checked:true},
+		{ id: 3, name: 'item3' ,checked:true},
+		{ id: 4, name: 'item4' ,checked:true}
+	];
+	const flipDurationMs = 300;
+	function handleDndConsider(e) {
+		items = e.detail.items;
+	}
+	function handleDndFinalize(e) {
+		items = e.detail.items;
+	}
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>Todo</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<main class="flex justify-center">
+	<section class="w-10/12 xl:w-8/12 2xl:w-6/12">
+		<h2 class="mb-6 mt-4 text-2xl font-bold">Todo List Section</h2>
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>TODO List</Card.Title>
+				<Card.Description>a list of things you have to-do.</Card.Description>
+			</Card.Header>
+			<CardContent class="min-h-96">
+				<div
+					class="flex flex-col gap-4"
+					use:dndzone={{ items, flipDurationMs,dropTargetStyle:{}}}
+					on:consider={handleDndConsider}
+					on:finalize={handleDndFinalize}
+				>
+					{#each items as item, index (item.id)}
+						<div class={item.checked ? "" : "hidden"} animate:flip={{ duration: flipDurationMs }}>
+							{#if item.checked}
+								<div transition:blur>
+									<TodoCard bind:checked={item.checked} title={item.name} />
+								</div>
+							{/if}
+						</div>
+					{/each}
+				</div>
+			</CardContent>
+		</Card.Root>
+	</section>
+</main>
